e25c8db5b5924c9e29dc34f2684cd8bf8bf7be61
Metinagan <metin.agan01@gmail.com>
2024-11-25 21:15:20 +0300

    Initial commit

---
+++ spring-core/src/main/java/com/meetinagan/services/UserService.java
@@ -0,0 +1,16 @@
+package com.meetinagan.services;
+
+import java.util.List;
+
+import com.meetinagan.model.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserService {
+	
+	private List<User> userList;
+	
+}
